2dee37a3e3ccbd3b8497d69261201964b375d9e4
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-29 10:30:06 -0700

    Brew formula update for smock version v0.1.21

--- smock.rb
+++ smock.rb
@@ -2,16 +2,16 @@
 class Smock < Formula
   desc "smock - a server mock"
   homepage "https://github.com/broothie/smock"
-  version "0.1.19"
+  version "0.1.21"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/broothie/smock/releases/download/v0.1.19/smock_0.1.19_darwin_amd64.tar.gz"
-    sha256 "15f08811532dd7e1de9ce2c9b1a73420a19af0bcf265d97d93681ecf437a24f3"
+    url "https://github.com/broothie/smock/releases/download/v0.1.21/smock_0.1.21_darwin_amd64.tar.gz"
+    sha256 "4132901874a8d1dec766fc6d6919d0819ce1c96a6009a158e677a8a521790fbf"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/broothie/smock/releases/download/v0.1.19/smock_0.1.19_linux_amd64.tar.gz"
-      sha256 "cc618bbbf4235476c2fa608221926e4767b83cb6262a08e6950b95cc45840ee0"
+      url "https://github.com/broothie/smock/releases/download/v0.1.21/smock_0.1.21_linux_amd64.tar.gz"
+      sha256 "2de09cf404c52b6a5813b004e7118e4ab3c4116108cb19ff9e908073c5941c9d"
     end
   end
 
